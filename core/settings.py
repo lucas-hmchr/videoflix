dfc00b8d13d1474f3f4601665419c70c51177a62
lucas <lucas.hamacher@ma-versand.de>
2026-06-15 12:09:43 +0200

    connect postgresql database and create requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "drf_db",
+        "USER": "drf_user",
+        "PASSWORD": "Ssg08Abyss!",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
